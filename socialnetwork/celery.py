4d58bbe9f7906d272dc38425e028a13c760d2c6a
Emma Nguyen <emma.nguyen.0701@gmail.com>
2022-07-27 15:44:05 -0400

    Added image for README.md

--- socialnetwork/celery.py
+++ socialnetwork/celery.py
@@ -3,9 +3,8 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialnetwork.settings')
 
-# app = Celery('socialapp', backend='redis://localhost' , broker='redis://localhost')
-app = Celery('socialapp', backend='redis://:pa7610f9aaa770e797ff9cdb4666ba63795759e917135f6c00ceb4a14b64ba5d4@ec2-3-218-215-107.compute-1.amazonaws.com:9380', 
-broker='redis://:pa7610f9aaa770e797ff9cdb4666ba63795759e917135f6c00ceb4a14b64ba5d4@ec2-3-218-215-107.compute-1.amazonaws.com:9380')
+app = Celery('socialapp', backend='redis://localhost' , broker='redis://localhost')
+
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
